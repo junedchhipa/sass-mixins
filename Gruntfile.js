ffe3b062b6327ba0da434da0d5d78d2595af22ca
Juned Chhipa <juned@ourphilosophy.co.uk>
2016-03-23 23:34:39 +0530

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Read docs here: http://gruntjs.com/configuring-tasks
+    sass: {
+      options: {
+        outputStyle: 'expanded',
+        sourceMap: false
+      },
+      dev: {
+        files: [{
+          'dist/mixins.css': 'mixins.scss'
+        }]
+      },
+      dev2: {
+        files: [{
+          'demo/css/style.css': 'demo/demo.scss'
+        }]
+      }
+    },
+    watch: {
+      files: ['resources/assets/sass/**/*.scss'],
+      tasks: ['sass']
+    },
+  });
+
+  // Load the plugins.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+
+  // Default task(s).
+  grunt.registerTask('default', [
+      'sass'
+  ]);
+
+};
